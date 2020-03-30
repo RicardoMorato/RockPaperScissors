72e699baf5783df6db1767168cf7b779e81c4c39
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-03-30 00:59:10 -0300

    Feature(project-base): Made all folders and main files needed. Added ESLint to the project. Added .gitignore.

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <p>Dale boy!</p>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
